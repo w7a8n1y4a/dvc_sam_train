a479de6af9d7eaa51f0ddf7ecc5bdef7a576a12c
Ivan Serebrennikov <admin@silberworks.com>
2024-04-03 14:13:39 +0000

    feat(ci): add valid entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,7 +6,9 @@ ls -la
 cd /app
 dvc pull
 dvc repro -f
+git config --global user.email "automatic@pepemoss.com"
+git config --global user.name "Automatic Pepemoss"
 git add .
 git commit -m "automatic"
-git push https://$CI_COMMIT_USERNAME:$CI_COMMIT_PASSWORD@git.pepemoss.com:universitat/ml/sam_train.git
+git push https://$CI_COMMIT_USERNAME:$CI_COMMIT_PASSWORD@git.pepemoss.com:universitat/ml/sam_train.git HEAD:$CI_COMMIT_BRANCH
 dvc push
